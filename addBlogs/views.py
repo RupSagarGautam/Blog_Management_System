da94f7b7cd0206062dcf18a5a24718ac731be3a8
RupSagarGautam <rupsagargautam@gmail.com>
2025-07-06 11:14:31 +0545

    Updated added blogs from backend and fetched data to frontend

--- addBlogs/views.py
+++ addBlogs/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 from addBlogs import models
 # Create your views here.
-
-def home(request):
-    blogs = models.addBlog.objects.all().order_by('-id')
-    return render(request, 'pages/blog.html', {'blogs': blogs})

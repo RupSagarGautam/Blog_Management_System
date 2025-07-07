582801e984607e8d899e616e3a997d9d0dd089d8
RupSagarGautam <rupsagargautam@gmail.com>
2025-07-07 11:10:46 +0545

    Pulled and pushed

--- addBlogs/models.py
+++ addBlogs/models.py
@@ -1,11 +1,14 @@
+
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from users.views import User
 
 class addBlog(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to= 'blog_images/')
     content = models.TextField()
-    author = models.CharField(max_length=50, default='Anonymous')
+    author = models.CharField(max_length=50, default='Anoynymous')
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):

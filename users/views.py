4c06d4391733dae84422502321196cf2f560ef4e
Rejina <rejinadangal4@gamil.com>
2025-07-18 13:03:01 +0545

    updated code

--- users/views.py
+++ users/views.py
@@ -27,9 +27,9 @@ def loginUser(request):
                 messages.success(request, "You have successfully logged in")
                 return redirect("/")
             else:
-                errors['password'] = "Invalid Password!"
+                errors['password'] = "password doesnot match"
         else:
-            errors['email'] = "User with this email does not exist."
+            errors['username'] = "username doesnot match"
 
         return render(request, 'pages/login.html', {'errors': errors})
     else:
@@ -136,7 +136,7 @@ def signupUser(request):
 @login_required(login_url="/auth/log-in")
 def logoutUser(request):
     logout(request)
-    messages.success(request, "You Have Successfully Logged Out")
+    messages.success(request, "You have successfully logged out")
     print("Logout Success")
     return redirect("/auth/log-in")
 
